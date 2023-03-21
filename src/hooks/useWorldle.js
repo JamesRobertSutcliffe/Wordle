20bcd5367e4d6102b5d17a13a86b4118d6c1190d
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-03-21 20:46:25 +0000

    Keyup handler pretty much complete(make all keys uppercase somehow?) - set current guess next

--- src/hooks/useWorldle.js
+++ src/hooks/useWorldle.js
@@ -5,12 +5,11 @@ const useWordle = (solution) => {
     const [turn, setTurn] = useState(0);
     const [currentGuess, setCurrentGuess] = useState('');
     const [guesses, setGuesses] = useState([]);
-    const [history, setHistory] = useState([]);
+    const [history, setHistory] = useState(["hello"]);
     const [isCorrect, setIsCorrect] = useState(false);
 
     const formatGuess = () => {
-
-
+        console.log(` current guess: ${currentGuess}`);
     };
 
     const addNewGuess = () => {
@@ -19,10 +18,24 @@ const useWordle = (solution) => {
     }
 
     const handleKeyup = ({ key }) => {
+        if (key === 'Enter') {
+            if (turn > 5) {
+                console.log('You have used all of your guesses!')
+                return;
+            } if (history.includes(currentGuess)) {
+                console.log("You already tried that word!")
+                return;
+            } if (currentGuess.length !== 5) {
+                (console.log("Guess must be 5 letters long!"))
+                return;
+            }
+            formatGuess();
+        }
+
         if (key === 'Backspace') {
             setCurrentGuess((prev => {
                 return prev.slice(0, -1)
-            }))
+            }));
         }
         if (/^[A-Za-z]$/.test(key)) {
             if (currentGuess.length < 5) {
@@ -32,7 +45,7 @@ const useWordle = (solution) => {
                 })
             }
         }
-    }
+    };
 
     return { turn, currentGuess, guesses, isCorrect, handleKeyup }
 
