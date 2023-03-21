be1b06d95263c8509722b2c63e8ec9063cd6ab64
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-03-21 19:36:54 +0000

    custom hook built for initial logic - develop more logic next

---
+++ src/hooks/useWorldle.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+const [turn, setTurn] = useState(0);
+const [currentGuess, setCurrentGuess] = useState('');
+const [guesses, setGuesses] = useState([]);
+const [history, setHistory] = useState([]);
+const [isCorrect, setIsCorrect] = useState(false);
+
+const useWordle = (solution) => {
+
+    const formatGuess = () => {
+
+
+    };
+
+    const addNewGuess = () => {
+
+
+    }
+
+    const handleKeyup = () => {
+
+    }
+
+    return { turn, currentGuess, guesses, isCorrect, handleKeyup }
+
+
+}
+
+export default useWordle;
