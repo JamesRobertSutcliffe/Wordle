24f94dd351afba8b4e1e393a836583e99382c344
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-03-22 19:58:33 +0000

    colours developed - develop rendering further next

--- src/components/board/grid.js
+++ src/components/board/grid.js
@@ -3,11 +3,11 @@ import Row from "./row";
 
 const Grid = ({ currentGuess, guesses, turn }) => {
 
+
     return (
         <div>
-
             {guesses.map((g, i) => {
-                return <Row key={i} />
+                return <Row key={i} guess={g} />
             })}
         </div>
 
